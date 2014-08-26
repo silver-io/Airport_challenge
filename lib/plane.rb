68d5818166eaf9c003f4e99dcd4ab3ba8680a3a8
javier <js@silveriofunk.com>
2014-08-26 03:47:01 +0100

    futher changes to plane.rb

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,24 +1,21 @@
 class Plane 
 
-	def initialize
-		@flying = true
-	end
-
+	
 	def flying?
 		@flying 
 	end
 
-	# def in_air_status?
-	# 	@flying
-	# end
+	def in_air_status?
+		@flying
+	end
 
-	# def take_off!
-	# 	@flying = true
-	# end
+	def take_off!
+		@flying = true
+	end
 
-	# def land!
-	# 	@flying = false
-	# end
+	def land!
+		@flying = false
+	end
 
 
 end
